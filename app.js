0a231f9d32cf3cc0042c4ab875a9753d491ffe2e
i-ihtesham <mohammedihtesham21@gmail.com>
2023-04-10 14:07:19 +0530

    added style

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text;
+}
+function errorHandler(error)
+{
+    console.log(error);
+    alert("Server is down, try again later!!");
+}
 function clickEventHandler(){
-    console.log("click");
-    console.log(textInput.value);
+    var inputText = textInput.value;
+    fetch(getTranslationURL(inputText)).
+    then(response => response.json()).
+    then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 }
 btnTranslate.addEventListener("click", clickEventHandler);
